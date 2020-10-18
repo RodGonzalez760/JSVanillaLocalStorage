24393f421f37bda89d0324c8c477ae668dfefaab
RodGonzalez760 <50188279+RodGonzalez760@users.noreply.github.com>
2020-10-18 15:40:47 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
-// Variables
+
 const formulario = document.querySelector('#formulario');
 const listaTweets = document.querySelector('#lista-tweets');
 let tweets = [];
 
-
-// EventListeners
 eventListeners();
 function eventListeners (){
 
-    // Cuando el usuario envía el tweet
     formulario.addEventListener('submit', agregarTweet);    
 
-    // cuando el documento está listo
     document.addEventListener('DOMContentLoaded', () => {
         tweets = JSON.parse( localStorage.getItem('tweets')) || [];
 
@@ -23,41 +19,35 @@ function eventListeners (){
 
 
 function agregarTweet(e) {
-    e.preventDefault() //Previene cualquier acción por defecto
+    e.preventDefault();
 
     const tweet = document.querySelector('#tweet').value;
 
     if (tweet === '') {
         mostrarError('No puedes insertar un tweet vacío');
-        return; //Detiene la ejecucion del código
+        return;
     }
 
-    // crea un objeto con la fecha actual en milisegundos y el contenido del tweet
     const tweetOBj = {
         id: Date.now(),
-        tweet // es lo mismo que tweet: tweet
+        tweet 
     }
-    // añadir al arreglo de tweets
+   
     tweets = [...tweets, tweetOBj];
-
-    //despues de agregar, creamos el Html
+    
     crearHtml();
 
-    // Reiniciar el formulario
     formulario.reset();
 }
 
-// Mostrar mensaje de error
 function mostrarError(error) {
     const mensajeError = document.createElement('p');
     mensajeError.textContent = error;
     mensajeError.classList.add('error');
 
-    //Insertarlo en el contenido
     const contenido = document.querySelector('#contenido');
     contenido.appendChild(mensajeError);
 
-    //Elimina la alerta
     setTimeout(() => {
         mensajeError.remove();    
     }, 3000);
@@ -71,7 +61,6 @@ function crearHtml() {
     if (tweets.length > 0) {
         tweets.forEach( tweet => {
 
-            // Crear Elementoa
             const btnEliminar = document.createElement('a');
             btnEliminar.classList.add('borrar-tweet');
             btnEliminar.innerText = 'X';
@@ -79,12 +68,10 @@ function crearHtml() {
             const li = document.createElement('li');
             li.innerText = tweet.tweet;
             
-            // Dar funcionalidad al elemento
             btnEliminar.onclick = () => {
                 borrarTweet(tweet.id);
             }
             
-            //Asignar elementos
             listaTweets.appendChild(li);
             li.appendChild(btnEliminar);
 
@@ -94,13 +81,12 @@ function crearHtml() {
     sincronizarStorage();
 }
 
-// Agrega los tweets actuales a LocalStorage
 function sincronizarStorage() {
     localStorage.setItem('tweets', JSON.stringify(tweets));
 }
 
 function borrarTweet(id) {
-    // se utiliza que sea distinto de id, ya wue filter debe crear un nuevo arreglo xon todos los tweets excepto el que estoy eliminando
+
     tweets = tweets.filter( tweet => tweet.id != id);
 
     crearHtml();
